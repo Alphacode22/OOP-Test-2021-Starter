40fe4e54bad9bdc5cdd309cff223baf0064dfd35
Alex Bang <Alex.ZBP2456@GMAIL.COM>
2021-04-21 22:15:54 +0100

    Finished drawing

--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -14,25 +14,22 @@ enum NoteType {
 
 public class ScoreDisplay extends PApplet
 {
-	//String score = "DEFGABcd";
-    String score = "D2E2F2G2A2B2c2d2";
-	//String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
+	//String score= "DEFGABcd";
+   
+    //String score = "D2E2F2G2A2B2c2d2";
+	String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
 	char[] scoreChars = score.toCharArray();
+
+	String scoreTypeString = "DEFGABcd";
+	char[] scoreTypes = scoreTypeString.toCharArray();
+	float[] scoreY = {400, 350, 290, 250, 210, 180,130,100};
+
 	
 	ArrayList<Note> notes = new ArrayList<Note>();
 	private float border;
 	private float noteSpacing;
 
-	//private boolean[] isSelected;
-
-	// public float leftMargin;
-	// public float margin;
-
-	HashMap<Character, Integer> locations = new HashMap<>();
-	
-
-
-
+	float section;
 
 	public void settings()
 	{
@@ -48,27 +45,12 @@ public class ScoreDisplay extends PApplet
 	{
 		loadScore();
 		printScore();
-		// leftMargin = height * 0.2f;
-		// margin = height * 0.05f;
-		border = 100;
-	    noteSpacing = 100;
+		border = height / 10;
+	    noteSpacing = height / 10;
 
-		locations.put(convert('D'), convertInt(20));
-		locations.put(convert('E'), convertInt(18));
-		locations.put(convert('F'), convertInt(16));
-		locations.put(convert('G'), convertInt(14));
-		locations.put(convert('A'), convertInt(12));
-		locations.put(convert('B'), convertInt(10));
-		locations.put(convert('c'), convertInt(8));
-		locations.put(convert('d'), convertInt(6));
-	}
-
-	public Character convert(char in){
-		return Character.valueOf(in);
-	}
-
-	public Integer convertInt(int in){
-		return Integer.valueOf(in);
+		section = (height - border)/scoreTypes.length ;
+		System.out.println(section);
+		
 	}
 
 	public void draw()
@@ -85,55 +67,20 @@ public class ScoreDisplay extends PApplet
 		char _note= ' ';
 		char _duration= ' ';
 
-		// //int stringLength = score.length();
-		// for(int i=0; i < scoreChars.length; i++){
-		// 	// if(i == 0 ){
-		// 	// 	_note = scoreChars[i];
-		// 	// 	//System.out.println(_note);
-		// 	// }else {
-		// 	// 	if(i % 2 == 0){
-		// 	// 		_note = scoreChars[i];
-		// 	// 		//System.out.println(_note);
-		// 	// 	}
-		// 	// 	else if(i % 2 == 1 ){
-		// 	// 		//System.out.println("My Turn");
-		// 	// 		_duration = scoreChars[i];
-		// 	// 		//System.out.println(_duration);
-		// 	// 		newNote = new Note(_note, Character.getNumericValue(_duration));
-		// 	// 		notes.add(newNote);
-		// 	// 	}
-		// 	// }
-		// }
-
-		// for(int i=0; i < scoreChars.length-1; i++){
-		// 	if(Character.isLetter(scoreChars[i]) && Character.isDigit(scoreChars[i+1])){
-		// 		_note = scoreChars[i];
-		// 	}else if(Character.isDigit(scoreChars[i]) && Character.isLetter(scoreChars[i-1])){
-		// 		_duration = scoreChars[i];
-		// 		newNote = new Note(_note, Character.getNumericValue(_duration));
-		// 		notes.add(newNote);
-		// 	}else if(Character.isLetter(scoreChars[i]) && Character.isDigit(scoreChars[i-1])) {
-		// 		_note = scoreChars[i];
-		// 		for(int j=i; j< notes.size(); j++){
-		// 			if(Character.isDigit(scoreChars[i+1])){
-		// 				_duration = scoreChars[i];
-		// 				break;
-		// 			}
-		// 		}
-		// 	}
-			
-		// }
 		//String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
 		for(int i=0; i < scoreChars.length-1; i++){
 			if(Character.isLetter(scoreChars[i]) && Character.isDigit(scoreChars[i+1])){
 				_note = scoreChars[i];
 				_duration = scoreChars[i+1];
+				newNote = new Note(_note, Character.getNumericValue(_duration));
+				notes.add(newNote);
 			}else if(Character.isLetter(scoreChars[i]) && !Character.isDigit(scoreChars[i+1])){
 				_note = scoreChars[i];
 				_duration = '1';
+				newNote = new Note(_note, Character.getNumericValue(_duration));
+				notes.add(newNote);
 			}
-			newNote = new Note(_note, Character.getNumericValue(_duration));
-			notes.add(newNote);
+			
 		}
 	}
 
@@ -178,40 +125,20 @@ public class ScoreDisplay extends PApplet
 			//text(tempNote.getNote(), j * offset, border + height/4);
 		}
 
-
-		//float section = map(1, 0, border, 0, width - border);
-		//float y = map(k, 9, 0 , border, height - noteSpacing - offset);
-		// float section = height - border;
-		// float offset = height / 20;
-		// section /= notes.size()- noteSpacing - offset ;
-
-		//String score = "DEFGABcd";
-	
 		//Display note
 		for(int k=0; k < notes.size(); k++){
 			Note tempNote = notes.get(k);
 			float x = map(k, 0, notes.size(), border, width - border);
-			//float y = map(k, 0, 10, border, height - noteSpacing);
 			float offset = height / 20;
 			//float y = map(k, 10, 0 , border, height - noteSpacing - offset);
-			//float y = map(k, 9, 0 , border, height - noteSpacing - offset);
-			//float y = map(k, 9, 0 , border, height - noteSpacing - offset);
-			//selectNote(x, y);
+
 		    float y=0;
 
-		
-			for(Map.Entry<Character, Integer> location : locations.entrySet()){
-				if(Character.valueOf(tempNote.getNote()).equals(location.getKey())){
-					System.out.println(location.getKey() + "uo");
-					System.out.println(Character.valueOf(tempNote.getNote()));
-					y = location.getValue();
-					break;
+			for(int i=0; i < scoreTypes.length; i++){
+				if(scoreTypes[i] == tempNote.getNote()){
+					y = scoreY[i];
 				}
 			}
-
-		
-			
-			
 
 			//Black Circle
 			noStroke();
